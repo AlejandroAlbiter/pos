035c5dfd30d6bc6875645cdea99cec803ce55b8c
AlejandroAlbiter <albiter.alejandro@corvusdata.com>
2023-12-02 14:38:05 -0600

    build(web): Añadio proyecto

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2023 <a href="https://www.youtube.com" target="_blank">Base de Datos </a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
